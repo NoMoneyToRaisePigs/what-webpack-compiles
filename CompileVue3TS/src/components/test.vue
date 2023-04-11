3d211772dce2596e36463099b2c375d60ddaffef
NoMoneyToRaisePigs <gaofan19901103@gmail.com>
2023-04-11 23:04:13 +0800

    Compile Vue3 + TS, ts-loader and babel-loader

--- CompileVue3TS/src/components/test.vue
+++ CompileVue3TS/src/components/test.vue
@@ -11,7 +11,7 @@
  const count = ref(10)
 
  const increaseCount = () => { 
-   count.value++
+   count.value += 2
  }
  </script>
  
--- CompileVue3TS/webpack.config.js
+++ CompileVue3TS/webpack.config.js
@@ -1,13 +1,14 @@
-const CompilerSfc = require('@vue/compiler-sfc')
-const parse = CompilerSfc.parse
-CompilerSfc.parse = (source, options) => {
-   return parse(source, {
-      ...options,
-      pad: true
-   })
-}
+// const CompilerSfc = require('@vue/compiler-sfc')
+// const parse = CompilerSfc.parse
+// CompilerSfc.parse = (source, options) => {
+//    return parse(source, {
+//       ...options,
+//       pad: true
+//    })
+// }
 
 const path = require('path');
+const Webpack = require('webpack');
 const { VueLoaderPlugin } = require('vue-loader')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -18,44 +19,47 @@ module.exports = {
     filename: '[name].js',
     chunkFilename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
-   //  devtoolModuleFilenameTemplate: (info) => {
-   //    let resPath = info.resourcePath.split(path.sep).join("/");
-   //    let isVue = resPath.match(/\.vue$/);
-   //    let isGenerated = info.allLoaders;
+    devtoolModuleFilenameTemplate: (info) => {
+      let resPath = info.resourcePath.split(path.sep).join("/");
+      let isVue = resPath.match(/\.vue$/);
+      let isGenerated = info.allLoaders;
       
-   //    let generated = `webpack-generated:///${resPath}?${info.hash}`;
-   //    let vuesource = `your-source:///${resPath}`;
+      let generated = `webpack-generated:///${resPath}?${info.hash}`;
+      let vuesource = `your-source:///${resPath}`;
       
-   //    return isVue && isGenerated ? generated : vuesource;
-   //   },
-     devtoolModuleFilenameTemplate: info => {
-      if (info.allLoaders === '') {
-        // when allLoaders is an empty string the file is the original source
-        // file and will be prefixed with src:// to provide separation from
-        // modules transpiled via webpack
-        const filenameParts = ['src://']
-        if (info.namespace) {
-          filenameParts.push(info.namespace + '/')
-        }
-        filenameParts.push(info.resourcePath.replace(/^\.\//, ''))
-        return filenameParts.join('')
-      } else {
-        // otherwise we have a webpack module
-        const filenameParts = ['webpack://']
-        if (info.namespace) {
-          filenameParts.push(info.namespace + '/')
-        }
-        filenameParts.push(info.resourcePath.replace(/^\.\//, ''))
-        const isVueScript = info.resourcePath.match(/\.vue$/) &&
-          info.query.match(/\btype=script\b/) &&
-          !info.allLoaders.match(/\bts-loader\b/)
-        if (!isVueScript) {
-          filenameParts.push('?' + info.hash)
-        }
-        return filenameParts.join('')
-      }
-    },
-    devtoolFallbackModuleFilenameTemplate: 'webpack:///[resource-path]?[hash]'
+      return isVue && isGenerated ? generated : vuesource;
+     },
+    //  devtoolModuleFilenameTemplate: info => {
+    //   if (info.allLoaders === '') {
+    //     // when allLoaders is an empty string the file is the original source
+    //     // file and will be prefixed with src:// to provide separation from
+    //     // modules transpiled via webpack
+    //     const filenameParts = ['src://']
+    //     if (info.namespace) {
+    //       filenameParts.push(info.namespace + '/')
+    //     }
+    //     filenameParts.push(info.resourcePath.replace(/^\.\//, ''))
+    //     return filenameParts.join('')
+    //   } else {
+    //     // otherwise we have a webpack module
+    //     const filenameParts = ['webpack://']
+    //     if (info.namespace) {
+    //       filenameParts.push(info.namespace + '/')
+    //     }
+    //     filenameParts.push(info.resourcePath.replace(/^\.\//, ''))
+    //     const isVueScript = info.resourcePath.match(/\.vue$/) &&
+    //       info.query.match(/\btype=script\b/) &&
+    //       !info.allLoaders.match(/\bts-loader\b/)
+    //     if (!isVueScript) {
+    //       filenameParts.push('?' + info.hash)
+    //     }
+    //     return filenameParts.join('')
+    //   }
+    // },
+   devtoolFallbackModuleFilenameTemplate: 'webpack:///[resource-path]?[hash]'
+   },
+   resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
    },
    module: {
       rules: [
@@ -71,28 +75,60 @@ module.exports = {
             ]
          },
          {
-            test: /\.tsx?$/,
-            use: [
-               {
-                  loader: 'ts-loader',
-                  options: {
-                      configFile: path.resolve(__dirname, './tsconfig.json'),
-                      appendTsSuffixTo: [/\.vue$/]
-                  }  
-               }
-            ],
-            exclude: /node_modules/,
-          },
+            test: /\.ts$/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  '@babel/preset-env',
+                  ['@babel/preset-typescript', {
+                    // 这个配置是为了处理.vue文件解析后的ts文件
+                    allExtensions: true
+                  }]
+                ]
+              }
+            }
+         },
+         
+         // {
+         //    test : /\.ts$/,      
+         //    use: [
+         //      'babel-loader',
+         //      {
+         //        loader: 'ts-loader',
+         //         options: {
+         //          configFile: path.resolve(__dirname, './tsconfig.json'),
+         //          appendTsSuffixTo: [/\.vue$/],
+         //        },
+         //      },
+         //    ],
+         //    exclude: /node_modules/,
+         //  },
+         
+         // {
+         //    test: /\.tsx?$/,
+         //    use: [
+         //       {
+         //          loader: 'ts-loader',
+         //          options: {
+         //              configFile: path.resolve(__dirname, './tsconfig.json'),
+         //              appendTsSuffixTo: [/\.vue$/]
+         //          }  
+         //       }
+         //    ],
+         //    exclude: /node_modules/,
+         //  },
       ],
    },
    plugins: [
       new VueLoaderPlugin(),
       new HtmlWebpackPlugin({
          template: './index.html'
-      })
+      }),
+      new Webpack.DefinePlugin({ __VUE_OPTIONS_API__: true, __VUE_PROD_DEVTOOLS__: true }),
    ],
   // devtool: 'eval-source-map',
-  devtool: 'cheap-module-source-map',
+  devtool: 'inline-source-map',
   optimization: {
     splitChunks: {
       cacheGroups: {
@@ -134,6 +170,6 @@ module.exports = {
       // },
     },
    devServer: {
-
+    
     },
 };
